f710e37a09f6eff9edf47058b2b43d3aeff76df5
stooggle <stuartodonnell@gmail.com>
2018-05-07 00:19:04 +0100

    Created Add/Remove/Count Guests and Get/Set Capacity in ConferenceRoom Class. All tests passing.

--- src/main/java/rooms/ConferenceRoom.java
+++ src/main/java/rooms/ConferenceRoom.java
@@ -1,4 +1,40 @@
 package rooms;
 
-public class ConferenceRoom {
+public class ConferenceRoom extends Room {
+
+
+    private String name;
+    private double roomRate;
+
+    public ConferenceRoom(String name, double roomRate, int capacity) {
+        super(capacity);
+        this.name = name;
+        this.roomRate = roomRate;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getRoomRate() {
+        return roomRate;
+    }
+
+    public void setRoomRate(double roomRate) {
+        this.roomRate = roomRate;
+    }
+
+
+
+
+
+
+
+
+
 }
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,2 +1,86 @@
+import guests.Guest;
+import org.junit.Before;
+import org.junit.Test;
+import rooms.BedRoom;
+import rooms.BedroomType;
+import rooms.ConferenceRoom;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceRoomTest {
+
+    ConferenceRoom conferenceRoom;
+
+    Guest guest1;
+    Guest guest2;
+    Guest guest3;
+    Guest guest4;
+
+    @Before
+    public void before(){
+
+        conferenceRoom = new ConferenceRoom("The Jocky Wilson Suite", 1000.00, 100);
+        guest1 = new Guest();
+        guest2 = new Guest();
+        guest3 = new Guest();
+        guest4 = new Guest();
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("The Jocky Wilson Suite", conferenceRoom.getName());
+
+        }
+
+    @Test
+    public void setName(){
+        conferenceRoom.setName("The Gary Mabbut Suite");
+        assertEquals("The Gary Mabbut Suite", conferenceRoom.getName());
+    }
+
+    @Test
+    public void hasRoomRate(){
+
+        assertEquals(1000.00, conferenceRoom.getRoomRate(),0.1);
+    }
+
+    @Test
+    public void setRoomRate(){
+        conferenceRoom.setRoomRate(750.00);
+        assertEquals(750.00, conferenceRoom.getRoomRate(),0.1);
+    }
+
+    @Test
+    public void addGuestToRoom(){
+        conferenceRoom.addGuest(guest1);
+        conferenceRoom.addGuest(guest2);
+        conferenceRoom.addGuest(guest3);
+        conferenceRoom.addGuest(guest4);
+
+        assertEquals(4, conferenceRoom.countGuestsInRoom());
+    }
+
+    @Test
+    public void removeGuestFromRoom(){
+        conferenceRoom.addGuest(guest1);
+        conferenceRoom.addGuest(guest2);
+        conferenceRoom.addGuest(guest3);
+        conferenceRoom.addGuest(guest4);
+
+        conferenceRoom.removeGuestFromRoom();
+        assertEquals(3, conferenceRoom.countGuestsInRoom());
+
+    }
+
+    @Test
+    public void canGetRoomCapacity(){
+
+        assertEquals(100, conferenceRoom.getCapacity());
+    }
+
+    @Test
+    public void canSetRoomCapacity(){
+        conferenceRoom.setCapacity(150);
+        assertEquals(150, conferenceRoom.getCapacity());
+    }
 }
